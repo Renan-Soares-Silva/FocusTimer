c36e13154f9543f573c1342ea794457e7bf505ae
Renan Soares <renan.soares.mkx@gmail.com>
2023-04-28 00:00:31 -0300

    timmer add function conwtdown

--- js/script.js
+++ js/script.js
@@ -22,10 +22,41 @@ const Controls = {
     Controls.buttonSoundOn.classList.add('hide');
     Controls.buttonSoundOff.classList.remove('hide');
   },
+  SetSoundOn() {
+    Controls.buttonSoundOff.classList.add('hide');
+    Controls.buttonSoundOn.classList.remove('hide');
+  },
+};
+const Timmer = {
+  minutesDisplay: document.querySelector('.min'),
+  segDisplay: document.querySelector('.seg'),
+
+  SetMin() {
+    minutes = prompt('Quantos minutos?');
+    Timmer.minutesDisplay.textContent = String(minutes).padStart(2, '0');
+  },
+  SetSeg() {
+    segunds = prompt('Quantos segundos?');
+    Timmer.segDisplay.textContent = segunds;
+  },
+  countDown() {
+    setTimeout(function () {
+      let seconds = Number(Timmer.segDisplay.textContent);
+
+      if (seconds <= 0) {
+        seconds = 60;
+      }
+
+      Timmer.segDisplay.textContent = String(seconds - 1).padStart(2, '0');
+
+      Timmer.countDown();
+    }, 1000);
+  },
 };
 
 Controls.buttonPlay.onclick = () => {
   Controls.ChangeToPauseAndStop();
+  Timmer.countDown();
 };
 Controls.buttonPause.onclick = () => {
   Controls.ChangeToPlay();
@@ -38,3 +69,15 @@ Controls.buttonStop.onclick = () => {
 Controls.buttonSoundOn.onclick = () => {
   Controls.SetSoundOff();
 };
+Controls.buttonSoundOff.onclick = () => {
+  Controls.SetSoundOn();
+};
+Controls.buttonsetTime.onclick = () => {
+  Timmer.SetMin();
+  Timmer.SetSeg();
+  Timmer.countDown();
+};
+
+//const changeClass = (element) => {
+//element.classList.remove('hide');
+//element.changeClass.add('hide')}
